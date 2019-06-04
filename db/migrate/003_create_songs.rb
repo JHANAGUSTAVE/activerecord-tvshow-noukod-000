8199db401416ad81e621622ae6586081c3411211
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-04 15:09:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_songs.rb
+++ db/migrate/003_create_songs.rb
@@ -1,5 +1,9 @@
-class AddSeasonToShows < ActiveRecord::Migration[5.2]
+class CreateSongs < ActiveRecord::Migration[4.2]
   def change
-     add_column :shows, :season, :string
+    create_table :songs do |t|
+      t.string :name 
+      t.integer :artist_id
+      t.integer :genre_id
+    end
   end
 end
