4dd3324ab8228f3bf3c4a61e31dc2519b280fa67
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-04 14:56:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,7 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :shows do |t|
+    create_table :artists do |t|
       t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
     end
   end
 end
